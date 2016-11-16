94b1e1b14dfd8bf3f6bcee9fa125916edb02c25b
Ben <eekej001@gmail.com>
2016-11-15 16:21:18 -0800

    first commit

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+
+  config.api_key = "14aa9b21c4ee5d359c9c400cd1a35add"
+  config.secret = "8b2369e788b27c6dcd66b644ab833104"
+  config.scope = "read_orders, read_products, write_products"
+  config.embedded_app = true
+end
